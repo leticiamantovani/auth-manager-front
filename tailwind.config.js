7c62e458ff4eb2a46c844d866c9497e863046d27
leticiamantovani <leticiamantovani159@gmail.com>
2025-05-15 11:01:34 -0300

    chore: add initial configs

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,jsx,ts,tsx}"
+    ],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#1E3A8A",    // dark blue
+          secondary: "#3B82F6",  // medium blue
+          accent: "#93C5FD"      // light blue
+        }
+      }
+    },
+    plugins: []
+  }
+  
